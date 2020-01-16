f0e03c158e3a028bd1cf109ea7d16da4d64f3f69
Muhammad Khattak <muhammadskhattak@mail.utoronto.ca>
2020-01-16 18:43:22 -0500

    Fix missing import error

--- fleja-react-next-multi-concept-template/fleja/pages/creative-blog.js
+++ fleja-react-next-multi-concept-template/fleja/pages/creative-blog.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 import Bannar from '../components/creative-blog/Bannar';
-import BlogBody from '../components/creative-blog/BlogBody';
 
 export class index extends Component {
     render() {
