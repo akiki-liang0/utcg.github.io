036db2a61a3d0f9be925e795d3a06df1e40f358f
Akiki Liang <47992181+akiki-liang0@users.noreply.github.com>
2020-01-20 18:11:49 -0500

    Past Events & stylistic fixes
    - Completion of past events page
    - stylistic fixes to "page-title-area"

---
+++ fleja-react-next-multi-concept-template/fleja/components/digital-agency-animation/VolunteersHeading.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class CalendarHeading extends Component {
+  render(){
+    return(
+      <div className="container pt-20">
+          <div className="section-title">
+              <span className="wow fadeInUp">Volunteers</span>
+    <h2 className="wow fadeInDown">Meet UTCG's Honoured Volunteers</h2>
+          </div>
+
+      </div>
+    );
+  }
+
+}
+
+export default CalendarHeading;
