ea95e0e58e55fc0a5a69b98c8173ad9535095d0f
Rikin <dolllyamar@gmail.com>
2020-01-23 20:06:34 -0500

    updated calendar modal, style

--- fleja-react-next-multi-concept-template/fleja/components/freelancer-portfolio-animation/BigCalendarComponent.js
+++ fleja-react-next-multi-concept-template/fleja/components/freelancer-portfolio-animation/BigCalendarComponent.js
@@ -3,10 +3,11 @@ import { render } from 'react-dom'
 import moment from 'moment'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import request from 'superagent'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
 // a localizer for BigCalendar
 const localizer = momentLocalizer(moment)
-
-require('react-big-calendar/lib/css/react-big-calendar.css')
 
 let CALENDAR_ID = process.env.CALENDAR_ID;
 let API_KEY = process.env.CALENDAR_API_KEY;
@@ -32,6 +33,8 @@ function getEvents (callback) {
                         start: new Date(event.start.dateTime || event.start.date),
                         end: new Date(event.end.dateTime || event.end.date),
                         title: event.summary,
+                        description: event.description,
+                        location: event.location,
                     }
                     events.push(ne)
                 }
@@ -40,6 +43,53 @@ function getEvents (callback) {
         }
     })
 }
+
+
+function printHi() {
+    console.log('hi');
+    return false;
+}
+
+class CustomEvent extends React.Component {
+    constructor(event) {
+        super();
+        this.state = {displayDetails: false};
+        this.event = event.event;
+        this.toggleShowDetails = this.toggleShowDetails.bind(this)
+    }
+
+    toggleShowDetails() {
+        if (!this.state.displayDetails) {
+            this.setState({displayDetails: true});
+        } else {
+            this.setState({displayDetails: false});
+        }
+        console.log(this.state.displayDetails);
+    }
+
+    render() {
+        let timeString = this.event.start.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        return (
+            <div className='rbc-event'>
+                <div className='rbc-event-content' onClick={this.toggleShowDetails}>
+                    <div className='rbc-event-title'>{this.event.title}</div>
+                    <div className='rbc-event-info'>{timeString}, {this.event.location}</div>
+                </div>
+                <Modal className='calendar-modal' show={this.state.displayDetails} onHide={this.toggleShowDetails}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.event.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>{timeString + "\n" + this.event.location}</div>
+                        <div dangerouslySetInnerHTML={{__html: this.event.description} /* dangerous!!! */}></div>
+                    </Modal.Body>
+                </Modal>
+            </div>
+            )
+    }
+}
+
+
 
 class BigCalendarComponent extends Component {
     state = {
@@ -55,14 +105,17 @@ class BigCalendarComponent extends Component {
     render () {
         return (
             <Calendar
+            id="big-calendar"
             localizer={localizer}
             style={{height: '600px'}}
             events={this.state.events}
             min={minTime}
             max={maxTime}
+            components={{event: CustomEvent}}
             />
         )
     }
 }
 
 export default BigCalendarComponent;
+
