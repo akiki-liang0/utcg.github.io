f73ddc3d3cb52baf7a9288495f6e513495d62c13
Rikin <dolllyamar@gmail.com>
2020-01-30 18:31:50 -0500

    made calendar default to agenda view on mobile, updated calendar styles

--- fleja-react-next-multi-concept-template/fleja/components/freelancer-portfolio-animation/BigCalendarComponent.js
+++ fleja-react-next-multi-concept-template/fleja/components/freelancer-portfolio-animation/BigCalendarComponent.js
@@ -5,21 +5,12 @@ import { Calendar, momentLocalizer } from 'react-big-calendar'
 import request from 'superagent'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import { isMobile } from "react-device-detect";
 
-// a localizer for BigCalendar
-const localizer = momentLocalizer(moment)
 
 let CALENDAR_ID = process.env.CALENDAR_ID;
 let API_KEY = process.env.CALENDAR_API_KEY;
-let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
-
-// earliest time shown on the calendar: 8:00 am
-let minTime = new Date(0);
-minTime.setHours(8);
-
-// latest time shown on the calendar: 10:00 pm
-let maxTime = new Date(0);
-maxTime.setHours(22);
+let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
 
 function getEvents (callback) {
     // get events from Google Calendar
@@ -41,13 +32,7 @@ function getEvents (callback) {
             })
             callback(events);
         }
-    })
-}
-
-
-function printHi() {
-    console.log('hi');
-    return false;
+    });
 }
 
 class CustomEvent extends React.Component {
@@ -64,7 +49,6 @@ class CustomEvent extends React.Component {
         } else {
             this.setState({displayDetails: false});
         }
-        console.log(this.state.displayDetails);
     }
 
     render() {
@@ -89,8 +73,6 @@ class CustomEvent extends React.Component {
     }
 }
 
-
-
 class BigCalendarComponent extends Component {
     state = {
         events: []
@@ -103,12 +85,29 @@ class BigCalendarComponent extends Component {
     }
 
     render () {
+        // a localizer for BigCalendar
+        const localizer = momentLocalizer(moment);
+
+        // earliest time shown on the calendar: 8:00 am
+        let minTime = new Date(0);
+        minTime.setHours(9);
+
+        // latest time shown on the calendar: 10:00 pm
+        let maxTime = new Date(0);
+        maxTime.setHours(21);
+
+        let dfv = 'month';
+        if (isMobile) {
+            dfv = 'agenda';
+        }
+
         return (
             <Calendar
             id="big-calendar"
             localizer={localizer}
             style={{height: '600px'}}
             events={this.state.events}
+            defaultView={dfv}
             min={minTime}
             max={maxTime}
             components={{event: CustomEvent}}
